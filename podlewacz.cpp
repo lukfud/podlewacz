45b038028cb312e444f2c2091f84eb34d93465a3
lukfud <l.fudalewski@gmail.com>
2025-02-22 07:50:08 +0100

    Update podlewacz.cpp

--- podlewacz.cpp
+++ podlewacz.cpp
@@ -60,6 +60,8 @@ Podlewacz::Podlewacz(const char *apiUrlValue) :
                                                        sslClient->available());
       }
       strBuffer = "";
+      bool headersEnded = false;
+      char lastChar = 0;
       while (sslClient->available()) {
         char c = sslClient->read();
         Serial.print(c);
@@ -67,6 +69,10 @@ Podlewacz::Podlewacz(const char *apiUrlValue) :
           continue;
         }
         if (c == '\n') {
+          if (strBuffer.length() == 0) {
+            headersEnded = true;
+            continue;
+          }
           SUPLA_LOG_DEBUG("# strBuffer: %s (%d)", strBuffer.c_str(), strBuffer.length());
           if (strBuffer.startsWith("HTTP")) {
             sscanf(strBuffer.c_str(), "HTTP/%*d.%*d %d", &httpStatusCode);
@@ -80,8 +86,10 @@ Podlewacz::Podlewacz(const char *apiUrlValue) :
             break;
           }
           strBuffer = "";
-        } else if (httpStatusCode == 200 && strBuffer.length() == 1) {
-          SUPLA_LOG_DEBUG("# strBuffer: %s (%d)", strBuffer.c_str(), strBuffer.length());
+        } else {
+          strBuffer += c;
+        }
+        if (headersEnded && isDigit(c)) {
           if ((strBuffer == "0") || (strBuffer == "1")) {
             SUPLA_LOG_DEBUG("# podlewa.cz - sprinklers status: %s", strBuffer);
             state = !strBuffer.toInt();
@@ -92,93 +100,13 @@ Podlewacz::Podlewacz(const char *apiUrlValue) :
             state = 0;
             setActionValue(state);
           }
-          break;
-        } else {
-          strBuffer += c;
         }
+        break;
       }
       if (!sslClient->connected()) {
         sslClient->stop();
       }
     }
-/*
-  if (!dataFetchInProgress) {
-    if (lastServerReadTime == 0 || millis() - lastServerReadTime > 
-                             (retryCounter > 0 ? 5000 : refreshRateSec*1000)) {
-      lastServerReadTime = millis();
-#ifdef ARDUINO_ARCH_ESP32
-      const char* root_ca = \
-      "-----BEGIN CERTIFICATE-----\n" \
-      "MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
-      "TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
-      "cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
-      "WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
-      "ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
-      "MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
-      "h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
-      "0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
-      "A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
-      "T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
-      "B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
-      "B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
-      "KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
-      "OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
-      "jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n" \
-      "qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
-      "rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
-      "HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n" \
-      "hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
-      "ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
-      "3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
-      "NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
-      "ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
-      "TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
-      "jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
-      "oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
-      "4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
-      "mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
-      "emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
-      "-----END CERTIFICATE-----\n";
-      sslClient.setCACert(root_ca);
-      sslClient.setHandshakeTimeout(120);
-#endif
-#ifdef ARDUINO_ARCH_ESP8266
-      sslClient.stop();
-      sslClient.setBufferSizes(2048, 512);
-      sslClient.setInsecure();
-#endif
-      int returnCode = sslClient.connect("podlewa.cz", 443);
-      if (returnCode) {
-        retryCounter = 0;
-        dataFetchInProgress = true;
-        connectionTimeoutMs = lastServerReadTime;
-        SUPLA_LOG_DEBUG("# podlewa.cz - succesful connect");
-        char buf[200];
-        strcpy(buf, "GET /status/");
-
-        strcat(buf, apiUrl);
-        //strcat(buf, "/");
-
-        Serial.print(F("# podlewa.cz - query: "));
-        Serial.println(buf);
-
-        strcat(buf, " HTTP/1.1");
-
-        sslClient.println(buf);
-        sslClient.println(F("Host: podlewa.cz"));
-        sslClient.println(F("Connection: close"));
-        sslClient.println();
-
-      } else {  // if connection wasn't successful, try few times
-        SUPLA_LOG_DEBUG("# failed to connect to podlewa.cz api, "
-                                                "return code: %d", returnCode);
-        retryCounter++;
-		state = 0;
-		setActionValue(2);
-      }
-    }
-  }
-*/
   }
 
   void Podlewacz::setActionValue(int value) {
@@ -186,10 +114,10 @@ Podlewacz::Podlewacz(const char *apiUrlValue) :
       switch(value) {
         default:
 	    case 0:
-          runAction(Supla::ON_EVENT_1);
+          runAction(Supla::ON_EVENT_2);
           break;
         case 1:
-          runAction(Supla::ON_EVENT_2);
+          runAction(Supla::ON_EVENT_3);
           break;
         case 2:
           runAction(Supla::ON_ERROR);
